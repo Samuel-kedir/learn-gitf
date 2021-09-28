985022914fcb85fcb627b9305b5619559ec5db62
samuel kedir <samuel.datros@gmail.com>
2021-09-28 22:51:04 +0300

    night mode activated

---
+++ script.js
@@ -0,0 +1,7 @@
+var mode = document.getElementById("mode");
+var html = document.querySelectorAll("html")[0];
+mode.addEventListener("click",(event)=>{
+   html.classList.toggle("mode");
+    
+})
+
